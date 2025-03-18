86819989dd90850876f9bf3d8596e56c54bf0da3
Maria Eduarda Carvalho <ra2457023>
2025-03-18 13:39:21 -0300

    celsius para fahrenheit

---
+++ primeira branch.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+
+main()
+{
+	setlocale(LC_ALL,"Portuguese");
+	float celsius;
+	printf("Digite uma temperatura em Celsius: ");
+	scanf("%f",&celsius);
+	printf("\nConversão de celsius para Fahrenheit: %.2f",(celsius * 9.0/5.0) + 32);
+	
+	
+	
+	
+	
+}
+
+
